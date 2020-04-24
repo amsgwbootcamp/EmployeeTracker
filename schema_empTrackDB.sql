c7fb6898322cb3af4e409d3b1c9182a19e190469
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-04-23 20:37:12 -0400

    Adding starting of database server

---
+++ schema_empTrackDB.sql
@@ -0,0 +1,27 @@
+#DROP DATABASE if exists empTrackDB;
+CREATE DATABASE empTrackDB;
+
+USE empTrackDB;
+
+CREATE TABLE department (
+id		int			auto_increment,
+name	varchar(30)	not null,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+id			int			auto_increment,
+first_name	varchar(30)	not null,
+last_name 	varchar(30) not null,
+role_id		int,
+manager_id	int,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+id				int			auto_increment,
+title			varchar(30),
+salary			decimal,
+department_id	int,
+PRIMARY KEY(id)
+);
